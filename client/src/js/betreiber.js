8f8428581aa77147ba375783fe3bbd9c746e9d64
OSCA98 <oscarschwarz98@gmail.com>
2021-11-16 17:38:37 +0100

    Kommentare, umstrukturierungen

--- client/src/js/betreiber.js
+++ client/src/js/betreiber.js
@@ -2,12 +2,12 @@ const button = document.getElementById('buttonBetreiber');
 button.addEventListener('click', loadOperator);
 
 
-//GoogleÜbersetzer => Betreiber = Operator
 
 /**
  * Wird aufgerufen, wenn Betreiberansicht geladen werden soll
  */
 function loadOperator () {
+  //GoogleÜbersetzer => Betreiber = Operator
   loadEmptyLayout();
 }
 
@@ -79,22 +79,73 @@ function createElement (tagName,attributes ) {
   let newElement = document.createElement(tagName);
   let classString = attributes.class;
   let styleString = attributes.style;
+  let text = attributes.text;
   let id = attributes.id
   if (id) {
-    newElement.setAttribute("id",id);
+    setAttribute(newElement, "id", id);
   }
   if (classString) {
     for (let singleClass of classString.split(" ")) {
-      newElement.classList.add(singleClass)
+      addClass(newElement,singleClass)
     }
       
   }
   if (styleString) {
-    newElement.style.cssText=styleString;
+    setStyle(newElement,styleString)
+  }
+  if(text) {
+    setText(newElement,text)
   }
   return newElement;
 }
 
+/**
+ * Definiert Text innerhalb eines Elements
+ * ACHTUNG:Nur für Elemente ohne Kinder verwenden
+ * @param {Element} element Element in welches ein Text eingefügt wird
+ * @param {String} text Text, welcher eingefügt wird
+ */
+function setText(element, text) {
+  element.innerHTML = text
+}
+
+/**
+ * Definiert Style-Property für ein Element
+ * @param {Element} element Element, für welches das Attribut "Style" definiert wird
+ * @param {String} styleString String der Form "cssAttribute:Vvlue;cssAttribute:value;..."
+ */
+function setStyle (element, styleString) {
+  element.style.cssText = styleString;
+}
+
+/**
+ * Definiert beliebiges Attribut für ein Element.
+ * Bereits bestehende Elemente werden überschrieben.
+ * @param {Element} element Element, für welches das Attribut definiert wird
+ * @param {String} key Attributname
+ * @param {String} value Wert des Attributs
+ */
+function setAttribute (element, key, value) {
+  element.setAttribute(key,value);
+}
+
+/**
+ * Weißt einem Element eine Klasse zu
+ * @param {Element} element Element, welchem die Klasse zugewiesen wird
+ * @param {String} classString Klasse als String, welche hinzugefügt wird
+ */
+function addClass (element, classString) {
+  element.classList.add(classString);
+}
+
+/**
+ * Entfernt Klasse vom Element
+ * @param {Element} element Element, von welchem die Klasse entfernt wird
+ * @param {String} classString Klasse als String, welche entfernt wird
+ */
+ function removeClass (element, classString) {
+  element.classList.remove(classString);
+}
 
 /**
  * Fügt ein Element in ein anderes ein.
