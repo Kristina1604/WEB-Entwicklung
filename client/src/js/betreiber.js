6f6a023542d448b26fb12ae88e0306966e22f9f6
OSCA98 <oscarschwarz98@gmail.com>
2021-11-18 00:20:19 +0100

    Formular aus JSON-Datei erstellen

--- client/src/js/betreiber.js
+++ client/src/js/betreiber.js
@@ -7,6 +7,73 @@ operatorButton.addEventListener('click', toggleMenu,);
 const customerButton = document.getElementById('customerButton');
 customerButton.addEventListener('click', toggleMenu);
 
+//Daten zum laden eines Formulars
+let testForm = {
+  title: "Ticketbestellung",
+  inputs: [
+    {
+      description : "Name",
+      placeholder:"Geben Sie ihren vollen Namen ein",
+      type : "text"
+    },
+    {
+      description: "Vorstellung",
+      placeholder:"Wählen Sie eine Vorstellung",
+      type: "select",
+      options: ["Alladin 5", "Batman vs KingKong "]
+    },
+    {
+      description:"Anzahl Tickets",
+      placeholder:"Wie viele Tickets wollen Sie bestellen?",
+      type:"number"
+    },
+
+  ]
+}
+
+/**
+ * Erstellt <form>-Element aus Datenobjekt
+ * @param {Object} json Object mit allen Informationen, um daraus eine <form> zu generieren
+ * @returns {HTMLElement}
+ */
+function createFormFromJSON (json) {
+  let idCounter = 0; //Zählvariable, um eindeutige IDs generieren zu können
+
+  //Erstellt einzelnen Inputabschnitt (Beschreibung des Inputs und Input selbst)
+  let createFormElement = function (inputJSON) {
+    const inputID = `${json.title}-input-${idCounter}`
+    idCounter++;
+
+    let wrapper = createElement("div",{class:"form-group"});
+    let label = createElement("label",{for:inputID, text:inputJSON.description});
+    let input;
+    switch (inputJSON.type) {
+      case "text":
+        input = createElement("input",{class:"form-control", type:"text", id:inputID, placeholder:inputJSON.placeholder});
+        break;
+      case "select":
+        input = createElement("select",{class:"form-control", id:inputID});
+        inputJSON.options.forEach(option => addElement(input,createElement("option",{text:option})))
+        break;
+      case "number":
+        input = createElement("input",{class:"form-control",type:"number",id:inputID});
+        break;
+    }
+    addElement(wrapper,label);
+    addElement(wrapper,input);
+    return wrapper;
+
+  }
+
+  //<form>-Element erstellen und füllen
+  let form = createElement("form",{class:"p-4",id:"form"});
+  for (let inputJSON of json.inputs) {
+    let inputElement = createFormElement(inputJSON);
+    addElement(form,createFormElement(inputJSON));
+  }
+  return form;
+}
+
 //Variablen, die den aktuellen Zustand der beiden 'Dropdowns' erfassen
 // c = customer
 // o = operator
@@ -18,7 +85,6 @@ let cOpen = false;
  * @param {Event} event Event welches Informationen über den 'Klick' des Benutzers enthält
  */
 function toggleMenu(event) {
-
   let c_or_o = event.originalTarget.id.charAt(0); // Evaluiert zu 'c' bei Klick auf Kunde, und zu 'o' bei Klick auf Betreiber
   let open = c_or_o == "c" ? cOpen : oOpen; //Übernimmt Booleanwert (auf=true,zu=false) des entsprechenden Dropdowns
   let subButtons = document.getElementsByClassName(`${c_or_o}SubButton`) //HTMLCollection aller Dropdownitems des entsprechenden Dropdownmenüs
@@ -49,8 +115,16 @@ function toggleMenu(event) {
   //Ein geöffneter Hauptbutton soll etwas heller als ein geschlossener sein
   let primaryButton = document.getElementById(c_or_o == "c" ? "customerButton" : "operatorButton");
    open ? removeClass(primaryButton,"openPrimaryButton") :  addClass(primaryButton,"openPrimaryButton"); 
+   
+   //Anzeigen der Form zu Demozwecken:
+   if (open) {
+    let inputField = document.getElementById("form");
+    inputField.parentNode.removeChild(inputField);
+   } else {
+     let formular = document.getElementById("formular");
+     addElement(formular,createFormFromJSON(testForm))
+   }
 }
-
 
 /* 
  * --------------------------------------------------------------- 
@@ -75,26 +149,24 @@ function clearElement (element) {
  * @param {Object} attributes OPTIONAL z.B {class:"d-flex row",style:"backgroundColor:red;"}
  * @returns {Element} newElement Das neue Element
  */
-function createElement (tagName,attributes ) {
+function createElement (tagName,attributes) {
   let newElement = document.createElement(tagName);
-  let classString = attributes.class;
-  let styleString = attributes.style;
-  let text = attributes.text;
-  let id = attributes.id
-  if (id) {
-    setAttribute(newElement, "id", id);
-  }
-  if (classString) {
-    for (let singleClass of classString.split(" ")) {
-      addClass(newElement,singleClass)
+  for (let prop in attributes) {
+    switch (prop) {
+    case "class":
+      for (let singleClass of attributes[prop].split(" ")) {
+        addClass(newElement,singleClass)
+      }
+      break;
+    case "style":
+      setStyle(newElement,attributes[prop])
+      break;
+    case "text":
+      setText(newElement,attributes[prop])
+      break;
+    default:
+      setAttribute(newElement, prop, attributes[prop]);
     }
-      
-  }
-  if (styleString) {
-    setStyle(newElement,styleString)
-  }
-  if(text) {
-    setText(newElement,text)
   }
   return newElement;
 }
@@ -125,8 +197,17 @@ function setStyle (element, styleString) {
  * @param {String} key Attributname
  * @param {String} value Wert des Attributs
  */
-function setAttribute (element, key, value) {
+function setAttribute (element, key, value = key) {
   element.setAttribute(key,value);
+}
+
+/**
+ * Entfernt beliebiges Attribut für ein Element.
+ * @param {Element} element Element, für welches das Attribut entfernt wird
+ * @param {String} key Attributname
+ */
+ function removeAttribute (element, key) {
+  element.removeAttribute(key);
 }
 
 /**
