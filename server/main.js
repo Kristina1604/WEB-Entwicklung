05ad88df558f2300518967ce43e6f30b646434c7
Kristina Heinz <kristina.heinz92@web.de>
2021-11-18 20:12:12 +0100

    POST Methoden erstellt

--- server/main.js
+++ server/main.js
@@ -21,21 +21,45 @@ db.authenticate().then(function () {
   console.log('connection failed: ' + error);
 });
 
+// Kinosaal anlegen
+api.post('/addKinosaal', function (req, res) {
+  console.log('i got a request!');
+  console.log(req.body);
 
+  Model.Kinosaal.create({
 
+    kinoname: req.body.kinoName,
+    gesamtsitze: req.body.sitzeKomplett
 
+  });
+});
 
+// Vorstellung anlegen
+api.post('/addVorstellung', function (req, res) {
+  console.log('i got a request!');
+  console.log(req.body);
 
+  Model.Vorstellung.create({
 
+    datum: req.body.kalendertag,
+    uhrzeit: req.body.zeit,
+    kinosaal: req.body.kinoSaal,
+    filmname: req.body.filmName
 
+  });
+});
 
+// Reservierung anlegen
+api.post('/addReservierung', function (req, res) {
+  console.log('i got a request!');
+  console.log(req.body);
 
-
-
-
-
-
-
+  Model.Reservierung.create({
+    vorstellung: req.body.filmtitel,
+    tickets: req.body.sitzplätze,
+    kundenname: req.body.nameKunde
+  });
+});
 
 // Starten des Servers an Port 8080
 api.listen(PORT, function () {
