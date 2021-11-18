07ce7f3cb68b925f4a7a421eabd52a412225dc9b
OSCA98 <oscarschwarz98@gmail.com>
2021-11-18 23:15:31 +0100

    Unterstützt das Anzeigen verschiedener (dynamischer) Formulare

--- client/src/js/betreiber.js
+++ client/src/js/betreiber.js
@@ -2,43 +2,183 @@
 
 
 //Eventlistener für die Hauptbuttons in der Navbar
-const operatorButton = document.getElementById('operatorButton');
-operatorButton.addEventListener('click', toggleMenu,);
-const customerButton = document.getElementById('customerButton');
-customerButton.addEventListener('click', toggleMenu);
-
-//Daten zum laden eines Formulars
-let testForm = {
-  title: "Ticketbestellung",
-  inputs: [
-    {
-      description : "Name",
-      placeholder:"Geben Sie ihren vollen Namen ein",
-      type : "text"
-    },
-    {
-      description: "Vorstellung",
-      placeholder:"Wählen Sie eine Vorstellung",
-      type: "select",
-      options: ["Alladin 5", "Batman vs KingKong "]
-    },
-    {
-      description:"Anzahl Tickets",
-      placeholder:"Wie viele Tickets wollen Sie bestellen?",
-      type:"number"
-    },
-
-  ]
+let primaryButtons = document.getElementsByClassName('primaryButton');
+for (let i = 0; i < primaryButtons.length ; i++) {
+  primaryButtons.item(i).addEventListener('click', toggleMenu)
 }
+
+//Eventlistener für Unterbuttons in der Navbar
+let subButtons = document.getElementsByClassName('subButton');
+for (let i = 0; i < subButtons.length ; i++) {
+  subButtons.item(i).addEventListener('click', switchSide)
+}
+
+
+/* 
+ * --------------------------------------------------------------- 
+ * -----------------------Globale Variablen-----------------------
+ * ---------------------------------------------------------------
+ */
+
+//Variablen, die den aktuellen Zustand der beiden 'Dropdowns' erfassen
+// c = customer
+// o = operator
+let O_OPEN = false;
+let C_OPEN = false;
+
+//Objekt um Enumwerte für currentSide zu imitieren
+//Werte müssen den zugehörigen IDs der Buttons in der NavBar entsprechen 
+const SITE = {
+  EMPTY: "leer",
+  VORSTELLUNG_ANLEGEN: "vorstellungAnlegen",
+  KINOSAAL_ANLEGEN: "kinosaalAnlegen",
+  TICKETS_RESERVIEREN: "ticketsReservieren"
+}
+Object.freeze(SITE);
+//Variable für aktuell geöffnete Seite
+let CURRENTSIDE = SITE.EMPTY;
+
+//Daten zum Laden eines Formulars. Property-Keys müssen IDs der zugehörgen Buttons / Enumvariable SITE entsprechen 
+const FORMULAR_TEMPLATES = {
+  ticketsReservieren : {
+    title: "Ticketbestellung",
+    inputs: [
+      {
+        description : "Name",
+        placeholder:"Geben Sie ihren vollen Namen ein",
+        type : "text"
+      },
+      {
+        description: "Vorstellung",
+        placeholder:"Wählen Sie eine Vorstellung",
+        type: "select",
+        options: ["Alladin 5", "Batman vs KingKong "]
+      },
+      {
+        description:"Anzahl Tickets",
+        placeholder:"Wie viele Tickets wollen Sie bestellen?",
+        type:"number"
+      },
+  
+    ]
+  },
+  kinosaalAnlegen : {
+    title: "Kinosaal anlegen",
+    inputs: [
+      {
+        description : "Name",
+        placeholder:"Geben Sie den Namen des Kinosaals ein",
+        type : "text"
+      },
+      {
+        description: "Anzahl Reihen",
+        placeholder:"Wählen Sie die Anzahl der Reihen",
+        type: "number"
+      },
+      {
+        description:"Anzahl Sitze pro Reihe",
+        placeholder:"Wählen Sie die Anzahl der Sitze pro Reihe",
+        type:"number"
+      },
+  
+    ]
+  },
+  vorstellungAnlegen : {
+    title: "Vorstellung anlegen",
+    inputs: [
+      {
+        description : "Name",
+        placeholder:"Geben Sie den Namen der Vorstellung ein",
+        type : "text"
+      },
+      {
+        description: "Irgendwelche anderen Eigenschaften",
+        placeholder:"Gucke die Tage mal nochmal über die Aufgabenstellung drüber",
+        type: "text"
+      },
+  
+    ]
+  }
+} 
+
+/* 
+ * --------------------------------------------------------------- 
+ * -------------------------Seiten-Events--------------------------
+ * ---------------------------------------------------------------
+ */
+
+/**
+ * Öffnet und schließt die beiden Dropdowns in der Navleiste
+ * @param {Event} event Event welches Informationen über den 'Klick' des Benutzers enthält
+ */
+function toggleMenu(event) {
+  let c_or_o = event.originalTarget.id.charAt(0); // Evaluiert zu 'c' bei Klick auf Kunde, und zu 'o' bei Klick auf Betreiber
+  let open = c_or_o == "c" ? C_OPEN : O_OPEN; //Übernimmt Booleanwert (auf=true,zu=false) des entsprechenden Dropdowns
+  let subButtons = document.getElementsByClassName(`${c_or_o}SubButton`) //HTMLCollection aller Dropdownitems des entsprechenden Dropdownmenüs
+
+  //HTMLCollection erlaubt kein forEach(), deshalb wandle ich es in ein normales Array um
+  let subButtonsArray = [];
+  for (let i = 0 ; i < subButtons.length ; i++) {
+    subButtonsArray.push(subButtons.item(i))
+  }
+
+  if (open) {
+    //Das angeklickte Menü ist offen => Menü schließen
+    subButtonsArray.forEach(i => {
+      removeClass(i,"active")
+      addClass(i,"inactive")
+    })
+  } else {
+    //Das angeklickte Menü ist geschlossen => Menü öffnen
+    subButtonsArray.forEach(i => {
+      removeClass(i,"inactive")
+      addClass(i,"active")
+    })
+  }
+
+  //Die richtige Booleanvariable muss noch geflipt werden
+  c_or_o == "c" ? C_OPEN = !C_OPEN : O_OPEN = !O_OPEN;
+
+  //Ein geöffneter Hauptbutton soll etwas heller als ein geschlossener sein
+  let primaryButton = document.getElementById(c_or_o == "c" ? "customerButton" : "operatorButton");
+   open ? removeClass(primaryButton,"openPrimaryButton") :  addClass(primaryButton,"openPrimaryButton"); 
+}
+
+/**
+ * Koordiniert "Seitenwechsel" (Richtiges Formular laden)
+ * @param {Event} event  Event beim Klick auf Unterbutton in der Navbar
+ * @returns 
+ */
+function switchSide(event) {
+  //Seite, die angeklickt wurde
+  let newSite = event.originalTarget.id;
+
+  //Klick auf Seite, die bereits offen ist, hat keine Wirkung
+  if (newSite == CURRENTSIDE){
+    return;
+  }
+
+  let formularWrapper = document.getElementById("formular");
+  //Alte Seite entfernen
+  if (CURRENTSIDE != SITE.EMPTY) {
+    formularWrapper.innerHTML = '';
+  }
+
+  //Neuer Seitenwert in globalen Variablen hinterlegen
+  CURRENTSIDE = newSite;
+
+  //Passendes Formular laden
+  addElement(formularWrapper,createFormFromJSON(FORMULAR_TEMPLATES[newSite]))
+}
+
 
 /**
  * Erstellt <form>-Element aus Datenobjekt
  * @param {Object} json Object mit allen Informationen, um daraus eine <form> zu generieren
  * @returns {HTMLElement}
  */
-function createFormFromJSON (json) {
+ function createFormFromJSON (json) {
   let idCounter = 0; //Zählvariable, um eindeutige IDs generieren zu können
-
   //Erstellt einzelnen Inputabschnitt (Beschreibung des Inputs und Input selbst)
   let createFormElement = function (inputJSON) {
     const inputID = `${json.title}-input-${idCounter}`
@@ -64,7 +204,6 @@ function createFormFromJSON (json) {
     return wrapper;
 
   }
-
   //<form>-Element erstellen und füllen
   let form = createElement("form",{class:"p-4",id:"form"});
   for (let inputJSON of json.inputs) {
@@ -74,57 +213,7 @@ function createFormFromJSON (json) {
   return form;
 }
 
-//Variablen, die den aktuellen Zustand der beiden 'Dropdowns' erfassen
-// c = customer
-// o = operator
-let oOpen = false;
-let cOpen = false;
 
-/**
- * Öffnet und schließt die beiden Dropdowns in der Navleiste
- * @param {Event} event Event welches Informationen über den 'Klick' des Benutzers enthält
- */
-function toggleMenu(event) {
-  let c_or_o = event.originalTarget.id.charAt(0); // Evaluiert zu 'c' bei Klick auf Kunde, und zu 'o' bei Klick auf Betreiber
-  let open = c_or_o == "c" ? cOpen : oOpen; //Übernimmt Booleanwert (auf=true,zu=false) des entsprechenden Dropdowns
-  let subButtons = document.getElementsByClassName(`${c_or_o}SubButton`) //HTMLCollection aller Dropdownitems des entsprechenden Dropdownmenüs
-
-  //HTMLCollection erlaubt kein forEach(), deshalb wandle ich es in ein normales Array um
-  let subButtonsArray = [];
-  for (let i = 0 ; i < subButtons.length ; i++) {
-    subButtonsArray.push(subButtons.item(i))
-  }
-
-  if (open) {
-    //Das angeklickte Menü ist offen => Menü schließen
-    subButtonsArray.forEach(i => {
-      removeClass(i,"active")
-      addClass(i,"inactive")
-    })
-  } else {
-    //Das angeklickte Menü ist geschlossen => Menü öffnen
-    subButtonsArray.forEach(i => {
-      removeClass(i,"inactive")
-      addClass(i,"active")
-    })
-  }
-
-  //Die richtige Booleanvariable muss noch geflipt werden
-  c_or_o == "c" ? cOpen = !cOpen : oOpen = !oOpen;
-
-  //Ein geöffneter Hauptbutton soll etwas heller als ein geschlossener sein
-  let primaryButton = document.getElementById(c_or_o == "c" ? "customerButton" : "operatorButton");
-   open ? removeClass(primaryButton,"openPrimaryButton") :  addClass(primaryButton,"openPrimaryButton"); 
-   
-   //Anzeigen der Form zu Demozwecken:
-   if (open) {
-    let inputField = document.getElementById("form");
-    inputField.parentNode.removeChild(inputField);
-   } else {
-     let formular = document.getElementById("formular");
-     addElement(formular,createFormFromJSON(testForm))
-   }
-}
 
 /* 
  * --------------------------------------------------------------- 
