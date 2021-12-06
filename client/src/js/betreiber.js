db97a1efa51f12def2879d88df0634e026fcd0c8
OSCA98 <oscarschwarz98@gmail.com>
2021-12-06 12:26:02 +0100

    Popups

--- client/src/js/betreiber.js
+++ client/src/js/betreiber.js
@@ -199,8 +199,7 @@ function switchSide(event) {
         break;
     }
     idCounter++;
-    addElement(wrapper,label);
-    addElement(wrapper,input);
+    addElements(wrapper,[label,input]);
     return wrapper;
 
   }
@@ -210,8 +209,89 @@ function switchSide(event) {
     addElement(form,createFormElement(inputJSON));
   }
   let submitButton = createElement("button", {text:"Absenden", type:'button', id:`submitButton-${json.buttonId}`});
+  submitButton.addEventListener('click',togglePopup)
   addElement(form,submitButton)
   return form;
+}
+
+/**
+ * Öffnet die Popupansicht (also ausgegrauter Hintergrund und Popup)
+ */
+function togglePopup () {
+
+  //Hintergrund ausgrauen, indem ein schwarzes Div über die komplette Seite gelegt wird, welches ein bisschen durchsichtig ist
+  let body = document.getElementById('body');
+  let blurPage = createElement('div',{class:'blurPage justify-content-center mx-auto', id: 'blurPage'})
+  //Ein klick auf den ausgegrauten Bereich schließt das Popup wieder
+  blurPage.addEventListener('click',closePopup)
+
+
+  //Popupdiv
+  let popupWrapper = createElement('div',{
+    class:'popupWrapper',
+    id: 'popupWrapper'});
+  
+  //Problem: Klick auf Popup löst Clickevent der blurPage (siehe Zeile 226) aus
+  //Grund: Klickevents werden standardmäßig an 'unterdrunterliegende' Elemente weitergereicht
+  //Lösung: event.stopPropagation() beendet das 'weiter reichen'
+  popupWrapper.addEventListener('click', e => e.stopPropagation())
+
+
+  //Unterelemente fürs Popup erstellen
+  let popupHeader = createElement('div',{class: 'popupHeader', id: 'popupHeader',});
+  let popupText = createElement('div',{class: 'popupText', id: 'popupText',text:'Vielen Dank'});
+  let popupFooter = createElement('div',{class: 'popupFooter', id: 'popupFooter',})
+  let acceptButton = createElement('button',{text:'Alles klar',class: 'acceptButton btn btn-primary'});
+
+  //Buttonclick soll das Popup schließen
+  acceptButton.addEventListener('click',closePopup)
+  
+  //Alles zusammenstecken
+  addElement(popupFooter,acceptButton);
+  addElements(popupWrapper,[popupHeader,popupText,popupFooter]);
+  addElement(blurPage,popupWrapper);
+  addElement(body,blurPage);
+  
+  //Die Position des Popups ist allerdings noch nirgendwo definiert.
+  //Die Logik dafür habe ich in eine eigene Funktion verlegt
+  adjustPopupPosition();
+
+  //Probem: Das Groß- und Kleinziehen des Browserfensters bei offenem Popup verschiebt unser Menu, die Popupposition bleibt allerdings gleich
+  //Grund: Bei Öffnen des Popups wird die Position 1x fest geladen, und bleibt dann immer gleich
+  //Lösung: Position neu berechnen, jedes mal wenn sich das Browserfenster verändert
+  window.addEventListener('resize', adjustPopupPosition)
+  
+}
+
+/**
+ * Platziert das Popup horizontal zentriert, oben anliegend, im Formularteil des Menüs
+ * Es wird dann noch um einen festen Wert (marginTop) nach unten geschoben
+ */
+function adjustPopupPosition () {
+
+  //X- bzw Y-Wert der view Ränder des Formularelements
+  let formularEdges = document.getElementById('formular').getBoundingClientRect();
+
+  //Genauer Mittelpunkt des Elements
+  let midPoint = {left : Math.round((formularEdges.right + formularEdges.left)/2)/*, top: Math.round((formularEdges.bottom + formularEdges.top)/2)*/};
+  
+  let popupWrapper = document.getElementById('popupWrapper')
+  let popupWidth = popupWrapper.offsetWidth;
+  let marginTop = 10;
+
+  //Positioniert Popup neu
+  popupWrapper.style.left = Math.round(midPoint.left - popupWidth/2) + 'px'
+  popupWrapper.style.top = Math.round(formularEdges.top + marginTop) + 'px'
+}
+
+
+/**
+ * Schließt Poopupansicht wieder 
+ */
+function closePopup (_event) {
+  removeElement(document.getElementById('blurPage'))
+  //Eventlistener ist nicht mehr nötig
+  window.removeEventListener('resize', adjustPopupPosition)
 }
 
 
@@ -259,6 +339,14 @@ function createElement (tagName,attributes) {
     }
   }
   return newElement;
+}
+
+/**
+ * Entfernt ein Element aus dem DOM-Baum
+ * @param {Element} element Das zu löschende Element 
+ */
+function removeElement (element) {
+  element.parentElement.removeChild(element)
 }
 
 /**
@@ -330,5 +418,17 @@ function addElement (outer, inner, position) {
     outer.appendChild(inner);
   } else {
     outer.insertBefore(inner,outer.children[position]);
+  }
+}
+
+/**
+ * Fügt in ein Element ein Array aus Elementen ein.
+ * Ganz analog zu addElement(), nur das mehrere Elemente gleichzeitig eingefügt werden können
+ * @param {Element} outer  Element, in das eingefügt wird
+ * @param {Array<Element>} arrayOfInner Array von Elementen die eingefügt werden
+ */
+function addElements (outer, arrayOfInner) {
+  for (let inner of arrayOfInner) {
+    addElement(outer,inner);
   }
 }
